b77664a7d448946044c2855e235f3dc3e4335679
teustoscano <ssstoscano@gmail.com>
2018-07-15 17:04:57 -0300

    Working with props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Person name="Teus" age="23"/>
       </div>
     );
   }
