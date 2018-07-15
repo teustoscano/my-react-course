c3d759eff04b24946808e9960f4fc2f8374ecc48
teustoscano <ssstoscano@gmail.com>
2018-07-15 18:17:15 -0300

    Working with binding methods

--- src/App.js
+++ src/App.js
@@ -11,12 +11,22 @@ class App extends Component {
   	]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
   	//console.log("Clcik");
   	this.setState({
   		persons: [
-  			{name: 'Matheus', age: 23},
+  			{name: newName, age: 23},
   			{name: 'Ronaldo', age: 33},
+  			{name: 'Karine', age: 21}
+  		]
+  	})
+  }
+
+  nameChangeHandler = (event) => {
+  	this.setState({
+  		persons: [
+  			{name: 'Teus', age: 23},
+  			{name: event.target.value, age: 33},
   			{name: 'Karine', age: 21}
   		]
   	})
@@ -25,8 +35,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler.bind(this, 'Toto')}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangeHandler}/>
+      <button onClick={() => this.switchNameHandler('Matheus')}>Switch Name</button>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const person = (props) => {
-	return <p>I'm {props.name} and I am {props.age} years old!</p>
+	return(
+		<div>
+			<p onClick={props.click}>I am {props.name} and I am {props.age} years old!</p>
+			<input type="text" onChange={props.changed} value={props.name}/>
+		</div>
+	) 
 };
 
 export default person;
