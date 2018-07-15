fef5546987150e1a5b6e35043838368f4bb566d1
teustoscano <ssstoscano@gmail.com>
2018-07-15 17:32:26 -0300

    Working with state

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+  	persons: [
+  		{name: 'Teus', age: 23},
+  		{name: 'Ronaldo', age: 33},
+  		{name: 'Karine', age: 21}
+  	]
+  }
+
+  switchNameHandler = () => {
+  	//console.log("Clcik");
+  	this.setState({
+  		persons: [
+  			{name: 'Matheus', age: 23},
+  			{name: 'Ronaldo', age: 33},
+  			{name: 'Karine', age: 21}
+  		]
+  	})
+  }
+
   render() {
     return (
       <div className="App">
-      <Person name="Teus" age="23"/>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <button onClick={this.switchNameHandler}>Switch Name</button>
       </div>
     );
   }
