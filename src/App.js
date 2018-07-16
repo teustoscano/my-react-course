47c1a4c3ef836de89d4a4f85467da46016df86ad
teustoscano <ssstoscano@gmail.com>
2018-07-15 22:52:03 -0300

    Using dynamic list on the component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
   	persons: [
-  		{name: 'Teus', age: 23},
-  		{name: 'Ronaldo', age: 33},
-  		{name: 'Karine', age: 21}
+  		{name: 'Teus', age: 23, id: 0},
+  		{name: 'Ronaldo', age: 33, id: 1},
+  		{name: 'Karine', age: 21, id: 2}
   	],
   	showPersons: false
   }
@@ -19,13 +19,20 @@ class App extends Component {
   	this.setState({persons: persons});
   }
 
-  nameChangeHandler = (event) => {
+  nameChangeHandler = (event, id) => {
+  	const personIndex = this.state.persons.findIndex(p => {
+  		return p.id === id;
+  	});
+
+  	const person = {...this.state.persons[personIndex]};
+
+  	person.name = event.target.value;
+
+  	const persons = [...this.state.persons];
+  	persons[personIndex] = person;
+
   	this.setState({
-  		persons: [
-  			{name: 'Teus', age: 23},
-  			{name: event.target.value, age: 33},
-  			{name: 'Karine', age: 21}
-  		]
+  		persons: persons
   	})
   }
 
@@ -50,7 +57,7 @@ class App extends Component {
       	{this.state.showPersons ? 
       	<div>
       		{this.state.persons.map((person, index) => {
-      			return <Person click={() => this.deletePersonHandler(index)} name={person.name} age={person.age}/>
+      			return <Person click={() => this.deletePersonHandler(index)} name={person.name} age={person.age} key={person.id} changed={(event) => this.nameChangeHandler(event, person.id)}/>
       		})}
       	</div>
       	: null}
