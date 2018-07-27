dac0c51468967922b94adc18ef41d274373cff63
teustoscano <ssstoscano@gmail.com>
2018-07-26 23:08:12 -0300

    Fix err from eject script

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import './Person.css';
 
 const person = (props) => {
 	return(
-		<div className="Person" style={style}>
+		<div className="Person">
 			<p onClick={props.click}>I am {props.name} and I am {props.age} years old!</p>
 			<input type="text" onChange={props.changed} value={props.name}/>
 		</div>
